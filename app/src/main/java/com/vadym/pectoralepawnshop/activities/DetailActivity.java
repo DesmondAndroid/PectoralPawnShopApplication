3f895546e1c0f1f5accd3ab71254f3e1f4d0f832
Vadym Shevchenko <spawnkiev@gmail.com>
2016-05-11 23:14:18 +0300

    add jsoup parser to detailed activity

--- app/src/main/java/com/vadym/pectoralepawnshop/activities/DetailActivity.java
+++ app/src/main/java/com/vadym/pectoralepawnshop/activities/DetailActivity.java
@@ -3,6 +3,7 @@ package com.vadym.pectoralepawnshop.activities;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
@@ -13,20 +14,16 @@ import com.vadym.pectoralepawnshop.database.DataBaseSimulation;
 import com.vadym.pectoralepawnshop.fragments.ForClientFragment;
 import com.vadym.pectoralepawnshop.fragments.HowItWorksFragment;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class DetailActivity extends Activity {
 
     public static final String EXTRA_TOPICNO = "topicNo";
     public static final String EXTRA_CALLEDFRAG = "calledFrag";
 
-    private class MyWebViewClient extends WebViewClient
-    {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url)
-        {
-            view.loadUrl(url);
-            return true;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,45 +31,29 @@ public class DetailActivity extends Activity {
         setContentView(R.layout.detail_activity);
 
         WebView mWebView = (WebView) findViewById(R.id.webView);
-        TextView contentView = (TextView) findViewById(R.id.contentView);
 
-        /* An instance of this class will be registered as a JavaScript interface */
-        class MyJavaScriptInterface
-        {
-            private TextView contentView;
+        try {
+            // Loading html page
+            Document document = Jsoup.connect("http://pectorale.com.ua/stat/4-prostyh-shaga").get();
+            // Remove header
+            document.getElementsByClass("header").remove();
+            // Remove footer
+            document.getElementsByClass("footer").remove();
+            document.getElementsByClass("empty_footer").remove();
+            // Remove block - subscription
+            document.getElementsByClass("subscription").remove();
+            // Remove main topics at the top page
+            document.select("div.container_inside > div.row > div").remove();
+            // Remove all links in the text
+            document.select("a").unwrap();
 
-            public MyJavaScriptInterface(TextView aContentView)
-            {
-                contentView = aContentView;
-            }
-
-            @SuppressWarnings("unused")
-            public void processContent(String aContent)
-            {
-                final String content = aContent;
-                contentView.post(new Runnable()
-                {
-                    public void run()
-                    {
-                        contentView.setText(content);
-                    }
-                });
-            }
+            mWebView.getSettings().setJavaScriptEnabled(true);
+            mWebView.loadData(document.toString(), "text/html", "utf-8");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-//        mWebView.setWebViewClient(new MyWebViewClient());
-        // включаем поддержку JavaScript
-        mWebView.getSettings().setJavaScriptEnabled(true);
 
-        mWebView.addJavascriptInterface(new MyJavaScriptInterface(contentView), "INTERFACE");
-        mWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                view.loadUrl("javascript:window.INTERFACE.processContent(document.getElementsByTagName('body')[0].innerText);");
-            }
-        });
-        // указываем страницу загрузки
-        mWebView.loadUrl("http://pectorale.com.ua/stat/4-prostyh-shaga");
     }
 
     @Override
