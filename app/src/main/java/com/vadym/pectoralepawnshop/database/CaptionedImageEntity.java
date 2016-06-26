8b5d66dd1cfaad45dab12b646ba0ef5bc1a5ece1
Vadym Shevchenko <spawnkiev@gmail.com>
2016-06-26 10:38:06 +0300

    add Department Entity and new objects to db

---
+++ app/src/main/java/com/vadym/pectoralepawnshop/database/CaptionedImageEntity.java
@@ -0,0 +1,28 @@
+package com.vadym.pectoralepawnshop.database;
+
+/**
+ * Created by Администратор on 25.06.2016.
+ */
+public class CaptionedImageEntity {
+
+    private String name;
+    private int imageResourceId;
+    private String url;
+
+    public CaptionedImageEntity(String name, int imageResourceId, String url) {
+        this.name = name;
+        this.imageResourceId = imageResourceId;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+    public String getUrl() {
+        return url;
+    }
+
+}
---
+++ app/src/main/java/com/vadym/pectoralepawnshop/database/DepartmentEntity.java
@@ -0,0 +1,53 @@
+package com.vadym.pectoralepawnshop.database;
+
+/**
+ * Created by Администратор on 25.06.2016.
+ */
+public class DepartmentEntity {
+
+    private String name;
+    private double coordinateX;
+    private double coordinateY;
+    private String city;
+    private String address;
+    private String[] telephoneNumbers;
+    private String hoursWorking;
+
+    public DepartmentEntity(String name, double coordinateX, double coordinateY, String city, String address, String[] telephoneNumbers, String hoursWorking) {
+        this.name = name;
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+        this.city = city;
+        this.address = address;
+        this.telephoneNumbers = telephoneNumbers;
+        this.hoursWorking = hoursWorking;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCoordinateX() {
+        return coordinateX;
+    }
+
+    public double getCoordinateY() {
+        return coordinateY;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String[] getTelephoneNumbers() {
+        return telephoneNumbers;
+    }
+
+    public String getHoursWorking() {
+        return hoursWorking;
+    }
+}
